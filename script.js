7f58ed3c3bb6e4dc19aa68d4990bcd0f1da8ce29
pkrzysiekk <pkrzysiek24@gmail.com>
2024-07-20 18:16:57 +0200

    Add human choice to the game

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ function getRandomInt(max)
 
 function getComputerChoice()
 {
-    const choices=['rock','paper','scissors']
-    return choices[getRandomInt(3)]
+    const ComputerChoices=['rock','paper','scissors']
+    return ComputerChoices[getRandomInt(3)]
+}
+function getHumanChoice()
+{   const choices=['rock','paper','scissors']
+    humanChoice=prompt("Hello! You are about to play rock paper scissors against the computer,what's your choice?")
+    if(choices.indexOf(humanChoice)===-1)
+    {
+        alert("Wrong choice!")
+    }
+    else
+    return humanChoice  
+    
+
 }
 console.log(getComputerChoice())
+console.log(getHumanChoice())

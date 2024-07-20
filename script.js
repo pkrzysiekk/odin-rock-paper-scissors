3a5f2efcba511dba89e529f6c6d49e83fbd2a22e
pkrzysiekk <pkrzysiek24@gmail.com>
2024-07-20 18:52:52 +0200

    Add function to play one round of the game
    Added function playRound that plays one round of
    the game and decides who won

--- script.js
+++ script.js
@@ -13,14 +13,56 @@ function getComputerChoice()
 function getHumanChoice()
 {   const choices=['rock','paper','scissors']
     humanChoice=prompt("Hello! You are about to play rock paper scissors against the computer,what's your choice?")
+    humanChoice=humanChoice.toLowerCase()
     if(choices.indexOf(humanChoice)===-1)
     {
         alert("Wrong choice!")
     }
     else
     return humanChoice  
+}
+function playRound(humanChoice,computerChoice)
+{
+    if(humanChoice===computerChoice)
+    {       
+        alert("Draw!")
+    }
+    else if(humanChoice==='rock' && computerChoice==='paper')
+    {
+        alert("Computer wins!")
+        computerScore++
+    }
+    else if(humanChoice==='rock' && computerChoice==='scissors')
+    {
+        alert("You win!")
+        humanScore++
+    }
+    else if(humanChoice==='paper' && computerChoice==='scissors')
+    {
+        alert("Computer wins!")
+        computerScore++
+    }
+    else if(humanChoice==='paper' && computerChoice==='rock')
+    {
+        alert("You win!")
+        humanScore++
+    }
+    else if(humanChoice==='scissors' && computerChoice==='rock')
+    {
+        alert("Computer wins!")
+        computerScore++
+    }
+    else if(humanChoice==='scissors' && computerChoice==='paper')
+    {
+        alert("You win!")
+        humanScore++
+    }
     
 
 }
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+let humanScore=0
+let computerScore=0
+humanChoice=getHumanChoice()
+computerChoice=getComputerChoice()
+playRound(humanChoice,computerChoice)
+alert(`Your score: ${humanScore} \n Computer score: ${computerScore}`)
